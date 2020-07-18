325d473e4e198a948c9b9bc5e98cec49adb016b9
Tiago Sanchez <tiagofsanchez@gmail.com>
2020-07-18 12:24:21 +0800

    chore: images in about page and form

--- src/components/familyPhoto.js
+++ src/components/familyPhoto.js
@@ -11,6 +11,14 @@ const FamilyPhotContainer = styled.div`
   grid-gap: 10px;
 `;
 
+const MartaContainer = styled.div`
+  height: 350px;
+  display: grid;
+  @media (max-width: 720px ) { 
+      height: 200px
+  }
+`;
+
 const KidsContainer = styled.div`
   display: grid;
   grid-gap: 10px;
@@ -24,10 +32,25 @@ const FamilyPhoto = () => {
   const diogo = useDiogoPhoto();
   return (
     <FamilyPhotContainer>
-      <Img fluid={marta} sx={{height: `350px`}} />
+      <MartaContainer>
+        <Img
+          fluid={marta}
+          aria-label="Picture of Marta"
+          alt="Picture of Marta"
+          objectFit="cover"
+        />
+      </MartaContainer>
       <KidsContainer>
-        <Img fluid={diogo} />
-        <Img fluid={monica} />
+        <Img
+          fluid={diogo}
+          aria-label="Picture of Diogo"
+          alt="Picture of Diogo"
+        />
+        <Img
+          fluid={monica}
+          aria-label="Picture of Monica"
+          alt="Picture of Monica"
+        />
       </KidsContainer>
     </FamilyPhotContainer>
   );
