d9478e61ef6d325999ea7846b9b2fc85c12093ea
Tiago Sanchez <tiagofsanchez@gmail.com>
2020-07-28 23:01:35 +0800

    feature: implementing the like counter for each post

--- src/gatsby-theme-tfs/components/pages/blogPostPage.js
+++ src/gatsby-theme-tfs/components/pages/blogPostPage.js
@@ -3,6 +3,7 @@ import { jsx } from "theme-ui";
 import ThemeBlogPostPage from "gatsby-theme-tfs/src/components/pages/blogPostPage";
 import Form from "../../../components/form";
 import styled from "@emotion/styled";
+import Palmas from "../../../components/palmas";
 
 const GridContainer = styled.div`
   display: grid;
@@ -13,6 +14,7 @@ const BlogPostPage = (props) => {
   return (
     <GridContainer>
       <ThemeBlogPostPage {...props} />
+      <Palmas title={props.title}/>
       <div sx={{ variant: `layout.container` }}>
         <h1 sx={{textAlign: `center`}}>Subscribe</h1>
         <Form />
