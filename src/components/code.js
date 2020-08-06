75188f5b79995fa740b8671e3903a9ead2c6792b
Tiago Sanchez <tiagofsanchez@gmail.com>
2020-08-06 15:14:08 +0800

    feature: add line highlighting to prism-react-renderer

--- src/components/code.js
+++ src/components/code.js
@@ -2,7 +2,9 @@ import React from "react";
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from "react-live";
 import theme from "prism-react-renderer/themes/nightOwl";
 import Highlight, { defaultProps } from "prism-react-renderer";
+import rangeParser from "parse-numeric-range";
 import Title from "../components/title";
+import '../css/code.css'
 
 const LiveCode = (props) => {
   return (
@@ -25,10 +27,28 @@ const getParams = (className = ``) => {
   );
 };
 
+const calculateLinesToHighlight = (meta) => {
+  const RE = /{([\d,-]+)}/;
+  if (RE.test(meta)) {
+    const strlineNumbers = RE.exec(meta)[1];
+    const lineNumbers = rangeParser(strlineNumbers);
+    return (i) => lineNumbers.includes(i + 1);
+  } else {
+    return () => false;
+  }
+};
+
+
+
 const SyntaxHiglight = (props) => {
   const className = props.children.props.className || "";
   const [language, { title = `` }] = getParams(className);
   const ifTitle = (title || language) && { marginTop: `0px` };
+  const metastring = props.children.props.metastring || ""
+
+  const shouldHighlightLine = calculateLinesToHighlight(metastring);
+
+  console.log(metastring);
   return (
     <Highlight
       {...defaultProps}
@@ -43,13 +63,18 @@ const SyntaxHiglight = (props) => {
           </Title>
           <div className="gatsby-highlight" data-language={language}>
             <pre className={className} style={{ ...style, ...ifTitle }}>
-              {tokens.map((line, i) => (
-                <div {...getLineProps({ line, key: i })}>
-                  {line.map((token, key) => (
-                    <span {...getTokenProps({ token, key })} />
-                  ))}
-                </div>
-              ))}
+              {tokens.map((line, i) => {
+                const lineProps = getLineProps({ line, key: i })
+                if (shouldHighlightLine(i)) { lineProps.className = `${lineProps.className} highlight-line` }
+                console.log(lineProps)
+                return (
+                  <div key={i} {...lineProps}>
+                    {line.map((token, key) => (
+                      <span {...getTokenProps({ token, key })} />
+                    ))}
+                  </div>
+                );
+              })}
             </pre>
           </div>
         </>
@@ -59,7 +84,6 @@ const SyntaxHiglight = (props) => {
 };
 
 const Code = (props) => {
-  console.log(props);
   if (props.children.props["react-live"]) {
     return <LiveCode {...props} />;
   } else {
