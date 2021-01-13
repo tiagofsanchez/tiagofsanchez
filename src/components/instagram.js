a839369ba38f6ffa7e47f25f4d3f76d910f10d07
Tiago Sanchez <tiagofsanchez@gmail.com>
2021-01-13 23:57:45 +0800

    post(instagram)

--- src/components/instagram.js
+++ src/components/instagram.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import styled from "@emotion/styled"
+import styled from "@emotion/styled";
 
 const Card = styled.img`
   justify-self: center;
@@ -7,14 +7,13 @@ const Card = styled.img`
   height: 300px;
   background-position: center;
   background-repeat: no-repeat;
-`
+`;
 
 const Grid = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
   grid-gap: 10px;
-`
-
+`;
 
 const url =
   'https://www.instagram.com/graphql/query/?query_hash=42323d64886122307be10013ad2dcc44&variables={"id":40346689,"first":6}';
